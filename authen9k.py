591253121a367696eb4f2d9670b39e8bd850086b
Denny Wright <dwright2@madisoncollege.edu>
2024-01-04 12:33:22 -0600

    Updated authen9k

--- authen9k.py
+++ authen9k.py
@@ -1,6 +1,6 @@
 import requests
 
-def getCookie(addr) :
+def getCookie(addr,name, pwd) :
 
 #NX REST API Authen See REST API Reference for format of payload below
 
@@ -8,8 +8,8 @@ def getCookie(addr) :
  
     payload= {"aaaUser" :
               {"attributes" :
-                   {"name" : "cisco",
-                    "pwd" : "cisco"}
+                   {"name" : name,
+                    "pwd" : pwd}
                }
           }
 
@@ -18,14 +18,21 @@ def getCookie(addr) :
     return response.json()["imdata"][0]["aaaLogin"]["attributes"]["token"]
 
 
+#Main
+
+
 
 #Get Session Cookie for NX switch. Change address below as needed
 
-address = '10.10.20.177'
+#address = '10.10.20.177'
+address = "10.10.20.40"
+user = "admin"
+passw = "RG!_Yw200"
 
 #Use the cookie below to pass in request. Cookie is good for 600 seconds
 
-cookie = getCookie(address)
+cookie = getCookie(address,user,passw)
+print(cookie)
 
 
 
