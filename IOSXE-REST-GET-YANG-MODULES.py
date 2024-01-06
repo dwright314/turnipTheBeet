16f94173dc0a5d2efb7ed0ab533c22e2ec9b87e4
Denny Wright <dwright2@madisoncollege.edu>
2024-01-05 19:20:59 -0600

    GET CONf YANG update

--- IOSXE-REST-GET-YANG-MODULES.py
+++ IOSXE-REST-GET-YANG-MODULES.py
@@ -1,10 +1,12 @@
 import requests
 
-url = "https://10.10.20.175:443/restconf/data/ietf-yang-library:modules-state"
+addr = "10.10.20.48"
+
+url = "https://"+addr+":443/restconf/data/ietf-yang-library:modules-state"
 
 
-username = 'cisco'
-password = 'cisco'
+username = 'developer'
+password = 'C1sco12345'
 payload={}
 headers = {
   'Content-Type': 'application/yang-data+json',
